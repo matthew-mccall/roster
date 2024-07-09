46145f721ba3d9b141d95635b3c20c5243436ac5
Matthew McCall <m-mccall+git@outlook.com>
2024-07-09 12:09:20 -0400

    Refactor NavigationBarWithCategory component, add categories file
    Navigation links in the NavigationBarWithCategory component have been replaced with a dynamic list driven by a new categories JSON file. This change streamlines the code and makes adding new categories easier, by just adding their details to the `categories.json` file.

--- apps/web/src/components/NavigationBar/NavigationBarWithCategory.tsx
+++ apps/web/src/components/NavigationBar/NavigationBarWithCategory.tsx
@@ -1,30 +1,28 @@
-'use client'
+'use client';
 
 import NavigationBarBase from './NavigationBarBase';
-import { SignedIn, UserButton } from '@clerk/nextjs';
+import { SignedIn } from '@clerk/nextjs';
 import Link from 'next/link';
 import { IconedNavLink } from '../IconedNavLink';
 import { usePathname } from 'next/navigation';
 
+import categories from '../../categories.json';
+
 export default function NavigationBarWithCategory() {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   // For some reason the Navbar component doesn't work
   return (
     <NavigationBarBase>
       <SignedIn>
-          <Link href={'/matching/roommates'} passHref legacyBehavior>
-            <IconedNavLink className={`nav-link ${pathname === '/matching/roommates' ? 'active' : ''}`} aria-current="page" icon={"bi-building"}>Roommates</IconedNavLink>
-          </Link>
-          <Link href={"/matching/dating"} passHref legacyBehavior>
-            <IconedNavLink className={`nav-link ${pathname === '/matching/dating' ? 'active' : ''}`} aria-current="page" icon={"bi-clipboard-heart"}>Dating</IconedNavLink>
-          </Link>
-          <Link href={'/matching/friends'} passHref legacyBehavior>
-            <IconedNavLink className={`nav-link ${pathname === '/matching/friends' ? 'active' : ''}`} aria-current="page" icon={"bi-people"}>Friends</IconedNavLink>
-          </Link>
-          <Link href={"/matching/study"} passHref legacyBehavior>
-            <IconedNavLink className={`nav-link ${pathname === '/matching/study' ? 'active' : ''}`} aria-current="page" icon={"bi-mortarboard"}>Study Group</IconedNavLink>
-          </Link>
+        {
+          Object.entries(categories).map(([key, value]) => (
+            <Link href={`/matching/${value.route}`} passHref legacyBehavior key={key}>
+              <IconedNavLink className={`nav-link ${pathname === `/matching/${value.route}` ? 'active' : ''}`}
+                             aria-current="page" icon={value.icon}>{key}</IconedNavLink>
+            </Link>
+          ))
+        }
       </SignedIn>
     </NavigationBarBase>
   );

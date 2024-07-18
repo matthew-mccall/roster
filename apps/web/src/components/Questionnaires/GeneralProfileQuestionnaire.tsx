34e1b95be4d1b1c1d3165aea7c6fc1cbee7b624b
Andrew <andrew@thenazareths.net>
2024-07-18 15:58:04 -0400

    Update views. Fixed GeneralProfileQuestionnaire.tsx form

--- apps/web/src/components/Questionnaires/GeneralProfileQuestionnaire.tsx
+++ apps/web/src/components/Questionnaires/GeneralProfileQuestionnaire.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Form, FormControl, FormGroup, FormLabel, FormSelect } from 'react-bootstrap';
+import { Form, FormControl, FormGroup, FormLabel, FormSelect, Stack } from 'react-bootstrap';
 import updateGeneralProfile from '../../actions/updateGeneralProfile';
 import SubmitButton from '../SubmitButton';
 import { GeneralProfile } from '@roster/common';
@@ -10,7 +10,7 @@ export default function GeneralProfileQuestionnaire({ generalProfile }: { genera
   let name, gender;
 
   if (generalProfile) {
-    ({ name, gender } = generalProfile);
+    ({ name, gender} = generalProfile);
   }
 
   return (
@@ -18,12 +18,12 @@ export default function GeneralProfileQuestionnaire({ generalProfile }: { genera
       <Form action={updateGeneralProfile}>
         <FormGroup className="mb-3" controlId="formFullName">
           <FormLabel>Full Name</FormLabel>
-          <FormControl placeholder="Enter your full name" name="formFullName" />
+          <FormControl placeholder="Enter your full name" name="formFullName" defaultValue={name} />
         </FormGroup>
 
         <FormGroup className="mb-3" controlId="formGender">
           <FormLabel>Gender Identity</FormLabel>
-          <FormSelect aria-label="Default select example" name={"formGender"}>
+          <FormSelect aria-label="Default select example" name={"formGender"} defaultValue={gender} >
             <option value={0}>Prefer not to say</option>
             <option value={1}>Male</option>
             <option value={2}>Female</option>
@@ -32,15 +32,31 @@ export default function GeneralProfileQuestionnaire({ generalProfile }: { genera
         </FormGroup>
         <FormGroup className="mb-3" controlId="formInterests">
           <FormLabel>Enter up to 3 of your top hobbies and interests:</FormLabel>
-          <FormControl placeholder="Hobby/Interest"  name="formInterest0" />
-          <FormControl placeholder="Hobby/Interest"  name="formInterest1" />
-          <FormControl placeholder="Hobby/Interest"  name="formInterest2" />
+          <Stack gap={2}>
+            <FormControl placeholder="Hobby/Interest"
+                         defaultValue={generalProfile && generalProfile.interests.length > 0 ? generalProfile.interests[0]: ""}
+                         name="formInterest0" />
+            <FormControl placeholder="Hobby/Interest"
+                         defaultValue={generalProfile && generalProfile.interests.length > 1 ? generalProfile.interests[1]: ""}
+                         name="formInterest1" />
+            <FormControl placeholder="Hobby/Interest"
+                         defaultValue={generalProfile && generalProfile.interests.length > 2 ? generalProfile.interests[2]: ""}
+                         name="formInterest2" />
+          </Stack>
         </FormGroup>
         <FormGroup className="mb-3" controlId="formDislikes">
           <FormLabel>Enter up to 3 of your dislikes:</FormLabel>
-          <FormControl placeholder="Dislike"  name="formDislikes0" />
-          <FormControl placeholder="Dislike"  name="formDislikes1" />
-          <FormControl placeholder="Dislike"  name="formDislikes2" />
+          <Stack gap={2}>
+            <FormControl placeholder="Dislike"
+                         defaultValue={generalProfile && generalProfile.dislikes.length > 0 ? generalProfile.dislikes[0]: ""}
+                         name="formDislikes0" />
+            <FormControl placeholder="Dislike"
+                         defaultValue={generalProfile && generalProfile.dislikes.length > 1 ? generalProfile.dislikes[1]: ""}
+                         name="formDislikes1" />
+            <FormControl placeholder="Dislike"
+                         defaultValue={generalProfile && generalProfile.dislikes.length > 2 ? generalProfile.dislikes[2]: ""}
+                         name="formDislikes2" />
+          </Stack>
         </FormGroup>
         <SubmitButton variant="primary" type="submit">
           Save

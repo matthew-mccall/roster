97e84c99e46146f3a4de2a115be97e0121e9c824
Andrew <andrew@thenazareths.net>
2024-07-20 19:52:54 -0400

    Merged conflicts and cleaned up code.

--- apps/web/src/app/matching/[category]/page.tsx
+++ apps/web/src/app/matching/[category]/page.tsx
@@ -22,6 +22,29 @@ export default async function Matching({ params }: { params: { category: string 
     notFound()
   }
 
+  await mongoose;
+  const account = await AccountModel.findById(userId).exec();
+
+  if (!account) {
+    return;
+  }
+
+  if (params.category === "roommates" && !account.roommateProfile) {
+    return (<RoommateProfileQuestionnaire />);
+  }
+
+  if (params.category === "dating" && !account.datingProfile) {
+    return (<p>No Questionnaire or Profile Available (Dating)</p>);
+  }
+
+  if (params.category === "friends" && !account.friendsProfile) {
+    return (<p>No Questionnaire or Profile Available (Friends)</p>);
+  }
+
+  if (params.category === "study" && !account.studyProfile) {
+    return (<p>No Questionnaire or Profile Available (Study Group)</p>);
+  }
+
   let user1: Account, user2: Account;
   // TODO: Fetch two random accounts from matching pool
 
@@ -44,21 +67,7 @@ export default async function Matching({ params }: { params: { category: string 
         }
       </Stack>
     )
-  }
-
-  await mongoose;
-  const account = await AccountModel.findById(userId).exec();
-
-  if (!account) {
-    return;
-  }
-
-  // console.log(params.category);
-
-  if (params.category === "roommates") {
-    if (!account.roommateProfile) {
-      return(<RoommateProfileQuestionnaire/>);
-    }
+  } else {
     return (<div className={'text-center'}>
       <SignedIn>
         <h1 className={'text-primary fw-semibold display-1'}>The show&apos;s over</h1>
@@ -66,49 +75,4 @@ export default async function Matching({ params }: { params: { category: string 
       </SignedIn>
     </div>)
   }
-
-  if (params.category === "dating") {
-    if (!account.datingProfile) {
-      return(<p>No Questionnaire or Profile Available</p>);
-    }
-    return (<div className={'text-center'}>
-      <SignedIn>
-        <h1 className={'text-primary fw-semibold display-1'}>The show&apos;s over</h1>
-        <p className={'lead'}>We ran out of people to show you. Check in later.</p>
-      </SignedIn>
-    </div>)
-  }
-
-  if (params.category === "friends") {
-    if (!account.friendsProfile) {
-      return(<p>No Questionnaire or Profile Available</p>);
-    }
-    return (<div className={'text-center'}>
-      <SignedIn>
-        <h1 className={'text-primary fw-semibold display-1'}>The show&apos;s over</h1>
-        <p className={'lead'}>We ran out of people to show you. Check in later.</p>
-      </SignedIn>
-    </div>)
-  }
-
-  if (params.category === "study") {
-    if (!account.studyProfile) {
-      return(<p>No Questionnaire or Profile Available</p>);
-    }
-    return (<div className={'text-center'}>
-      <SignedIn>
-        <h1 className={'text-primary fw-semibold display-1'}>The show&apos;s over</h1>
-        <p className={'lead'}>We ran out of people to show you. Check in later.</p>
-      </SignedIn>
-    </div>)
-  }
-
-  return (
-    <div className={"text-center"}>
-      <SignedIn>
-        <h1 className={"text-primary fw-semibold display-1"}>The show&apos;s over</h1>
-        <p className={'lead'}>We ran out of people to show you. Check in later.</p>
-      </SignedIn>
-    </div>
-  )
 }

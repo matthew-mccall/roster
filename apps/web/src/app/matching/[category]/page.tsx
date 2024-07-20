bed2f57857f86bd91cf78796d4ba4f4ae106d8e0
Matthew McCall <m-mccall+git@outlook.com>
2024-07-20 14:48:15 -0400

    Update Matching function to handle incomplete profiles
    The Matching function in the category page now awaits the creation or retrieval of an account before proceeding. If an account is not found or if its general profile is incomplete, an appropriate message is displayed to the user. Furthermore, the conditions under which different displays are rendered to the user have been revised and reorganized.

--- apps/web/src/app/matching/[category]/page.tsx
+++ apps/web/src/app/matching/[category]/page.tsx
@@ -1,11 +1,14 @@
 import { SignedIn } from '@clerk/nextjs';
 import categories from '../../../categories.json'
 import { notFound } from 'next/navigation';
-import { Card, CardBody, Stack } from 'react-bootstrap';
+import { Alert, AlertLink, Card, CardBody, Stack } from 'react-bootstrap';
 import { Account } from '@roster/common';
 import Button from 'react-bootstrap/Button';
+import getOrCreateAccount from '../../../lib/getOrCreateAccount';
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
 
-export default function Matching({ params }: { params: { category: string } })
+export default async function Matching({ params }: { params: { category: string } })
 {
   const categoryRoutes = Object.entries(categories).map(([, value]) => {
     return value.route
@@ -23,28 +26,39 @@ export default function Matching({ params }: { params: { category: string } })
     // TODO: Calculate ELO, update roster entries
   }
 
-  if (user1 && user2) {
+  const account = await getOrCreateAccount({ required: true })
+
+  if (!account || !account.generalProfile) {
     return (
-      <Stack direction={"horizontal"} gap={3} className={'justify-content-center'}>
-        {
-          [user1, user2].map((account, key) => (
-            <Card key={key}>
-              <CardBody>
-                <Button onClick={() => submitPreference(account._id)}>Like</Button>
-              </CardBody>
-            </Card>
-          ))
-        }
-      </Stack>
+      <Container>
+        <Alert variant={"secondary"}>Please complete the <Link href={"/"} passHref legacyBehavior><AlertLink>general questionnaire</AlertLink></Link> first</Alert>
+      </Container>
+    )
+  }
+
+  if (!user1 || !user2) {
+    return (
+      <div className={"text-center"}>
+        <SignedIn>
+          <h1 className={"text-primary fw-semibold display-1"}>The show&apos;s over</h1>
+          <p className={'lead'}>We ran out of people to show you. Check in later.</p>
+        </SignedIn>
+      </div>
     )
   }
 
   return (
-    <div className={"text-center"}>
-      <SignedIn>
-        <h1 className={"text-primary fw-semibold display-1"}>The show&apos;s over</h1>
-        <p className={'lead'}>We ran out of people to show you. Check in later.</p>
-      </SignedIn>
-    </div>
+    <Stack direction={"horizontal"} gap={3} className={'justify-content-center'}>
+      {
+        [user1, user2].map((account, key) => (
+          <Card key={key}>
+            <CardBody>
+              <Button onClick={() => submitPreference(account._id)}>Like</Button>
+            </CardBody>
+          </Card>
+        ))
+      }
+    </Stack>
   )
+
 }
